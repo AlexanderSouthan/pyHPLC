ac3756e05b78e37318677fe3f4931a7bc7549e09
AlexanderSouthan <72788772+AlexanderSouthan@users.noreply.github.com>
2021-12-02 11:44:42 +0100

    Reorganised for packaging

---
+++ tests/test_import.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Nov 23 19:55:00 2021
+
+@author: Alexander Southan
+"""
+
+import unittest
+
+from src.pyHPLC.hplc_data import hplc_data
+
+
+class TestNonlinearRegression(unittest.TestCase):
+
+    pass
