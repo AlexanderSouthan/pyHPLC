fafea509f2f72b21b64e40128b7707407b3a12f8
AlexanderSouthan <72788772+AlexanderSouthan@users.noreply.github.com>
2021-12-02 12:00:34 +0100

    Added install requirements

--- setup.py
+++ setup.py
@@ -3,5 +3,6 @@ from setuptools import setup, find_packages
 setup(
     name='pyHPLC',
     version='0.0.1',
-    packages=find_packages(where='src')
+    packages=find_packages(where='src'),
+    install_requires=['numpy', 'pandas', 'matplotlib', 'PyQt5', 'pyAnalytics', 'pyDataFitting', 'pyPreprocessing', 'tqdm'],
 )
